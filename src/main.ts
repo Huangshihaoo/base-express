3b0917aa47075fbbc19268601a5448b217d5bd93
haoo <huangshihao1122@163.com>
2024-08-10 16:12:19 +0800

    准备链接数据库

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import express from 'express'
 import routerRegister from './routers'
 import dotenv from 'dotenv'
+import * as bodyParser from "body-parser";
+
 
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` }) // 环境变量
 
 const { PORT } = process.env
 
 const app = express()
+app.use(bodyParser.json())
 
 // 注册路由
 routerRegister(app);
